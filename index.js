f6721ac4aa9396b6906f245f7f352c39289e9c97
Pedro Henrique Teles <pedrohenriqueteles732@gmail.com>
2024-06-07 20:57:29 -0300

    Pagina inicial sendo criada, tabela jquery do datatables em desenvolvimento

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-$(document).ready(onInit);
-
-function onInit() {
-    
+function listarColaboradores() {
     $.ajax({
-        url: "http://localhost:8080/api/colaborador/cont",
-        type: "get",
-        dataType: "json",
-        success: function (res) {
-            $("#div-total-alunos").html(res);
+        url: 'http://localhost:8080/api/colaborador',
+        type: 'get',
+        dataType: 'json',
+        success: function (result) {
+            console.log(result);
+            var html = '';
+            $.each(result, function (i, data) {
+                var datetime = new Date(data.dataNascimento);
+                var date = datetime.toISOString().split('T')[0];
+                html += `<tr><td>` + data.name + `</td>`;
+                html += `<td>` + data.email + `</td>`;
+                html += `<td>` + data.cpf + `</td>`;
+                html += `<td>` + data.telefone + `</td>`;
+                html += `<td>` + data.salario + `</td>`;
+                html += `<td>` + data.areaInteresse + `</td>`;
+                html += `<td>` + data.cargaHoraria + `</td>`;
+                html += `<td>` + date + `</td>`;
+            });
+
+            $("#TableColaboratorBody").html(html);
+
+            // Inicialize a tabela como uma tabela DataTables aqui
+            $('#TableColaborator').DataTable();
         }
     });
-
 }
+
+$(document).ready(listarColaboradores);
